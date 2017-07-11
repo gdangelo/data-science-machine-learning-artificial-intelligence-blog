543c3862ae701a28135599e624d5c3cf4819a731
Grégory D'Angelo <dangelo.gregory@gmail.com>
2017-07-11 16:10:52 +0200

    feat: Add intro section to homepage

---
+++ theme/static/js/home.js
@@ -0,0 +1,38 @@
+$(function(){
+
+	/*
+   * ----------------------------------------------------------------------------------------
+   *  TYPEWRITER JS
+   * ----------------------------------------------------------------------------------------
+   */
+	$("#typed").typed({
+		strings: ["a Data Scientist.^500", "a Machine Learning Engineer.^1000", "Greg."],
+		typeSpeed: 0,
+    callback: function() {
+      $(".sub-tagline").css("opacity", "1");
+      $(".sub-tagline").css("margin", "0");
+      $(".tagline").css("margin", "0");
+    }
+	});
+
+	/*
+   * ----------------------------------------------------------------------------------------
+   *  PARTICLES JS
+   * ----------------------------------------------------------------------------------------
+   */
+  // particlesJS.load(@dom-id, @path-json, @callback (optional));
+  particlesJS.load('particles-js', "theme/particlesjs-config.json");
+
+	/*
+   * ----------------------------------------------------------------------------------------
+   *  SMOOTH SCROLL JS
+   * ----------------------------------------------------------------------------------------
+   */
+  $(document).on('click', '.smooth-scroll', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+  });
+
+});
