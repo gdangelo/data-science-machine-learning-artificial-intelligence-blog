a0051b2333727349f4ef38de447f6912eda33300
Grégory D'Angelo <dangelo.gregory@gmail.com>
2017-07-11 15:53:10 +0200

    feat: Create main navbar

--- theme/static/js/app.js
+++ theme/static/js/app.js
@@ -1,2 +1,14 @@
-/* Custom scripts
--------------------------------------------------- */
+/*
+ * ----------------------------------------------------------------------------------------
+ *  SEARCH BAR
+ * ----------------------------------------------------------------------------------------
+ */
+$(document).on('click', '.search-button', function(event){
+  event.preventDefault();
+  $(".search-wrapper").addClass("search-active");
+  $(".search-input").focus();
+});
+$(document).on('click', '.search-close', function(event){
+  event.preventDefault();
+  $(".search-wrapper").removeClass("search-active");
+});
